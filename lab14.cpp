91a7bc21a3e25233059c6c614775a5b1f4cc2726
Your Name <brightsy04@gmail.com>
2026-03-01 19:07:13 -0800

    lab14-3

--- lab14.cpp
+++ lab14.cpp
@@ -1,5 +1,6 @@
 // COMSC-210 | lab14 |Seoyeon An
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class Color {
@@ -18,11 +19,35 @@ public:
     void setRed(int r) { red = r; }
     void setGreen(int g) { green = g; }
     void setBlue(int b) { blue = b; }
+
+    void print() const {
+        cout << left
+             << setw(6) << "R:" << red
+             << setw(6) << "G:" << green
+             << setw(6) << "B:" << blue
+             << endl;
+    }
 };
 
 
 
 int main() {
     cout << "Color Assignment";
+
+    Color c1;
+    c1.setRed(255);
+    c1.setGreen(0);
+    c1.setBlue(0);
+
+    Color c2;
+    c2.setRed(0);
+    c2.setGreen(255);
+    c2.setBlue(0);
+
+    Color c3;
+    c3.setRed(0);
+    c3.setGreen(0);
+    c3.setBlue(255);
+    
     return 0;
 }
