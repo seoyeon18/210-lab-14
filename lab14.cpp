d32802db9838510b8c568eda5559e39df8c6b388
Your Name <brightsy04@gmail.com>
2026-03-01 18:46:38 -0800

    lab14-1

---
+++ lab14.cpp
@@ -0,0 +1,15 @@
+// COMSC-210 | lab14 |Seoyeon An
+#include <iostream>
+using namespace std;
+
+class Color {
+private:
+    int red;
+    int green;
+    int blue;
+};
+
+int main() {
+    cout << "Color Assignment";
+    return 0;
+}
